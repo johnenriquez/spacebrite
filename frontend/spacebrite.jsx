05244133b472c02f5324e9490c639a93072c8bb8
John Enriquez <john.enriquez@gmail.com>
2020-01-29 16:10:50 -0800

    sets up redux/store structure

--- frontend/spacebrite.jsx
+++ frontend/spacebrite.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+
+const store = configureStore();
+
+window.getState = store.getState;
+window.dispatch = store.dispatch;
+
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(
         <h1>Spacebrite has launched! 🚀</h1>,
