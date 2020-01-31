2ad4f4ebcf4c1c5f44f2d45c2b6aba684f71beed
John Enriquez <john.enriquez@gmail.com>
2020-01-30 16:24:07 -0800

    sets up store and router on root component

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <h1>🚀 Spacebrite has launched! 🚀</h1>
+    </div>
+);
+
+export default App;
--- frontend/spacebrite.jsx
+++ frontend/spacebrite.jsx
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, logout, signup } from './actions/session_actions';
 import configureStore from './store/store';
+import Root from './components/root';
 
 const store = configureStore();
 
-window.dispatch = store.dispatch;
-window.login = login;
-window.logout = logout;
-window.signup = signup;
-
-
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(
-        <h1>Spacebrite has launched! 🚀</h1>,
+        <Root store={store} />,
         document.getElementById('root')
     );
 });
