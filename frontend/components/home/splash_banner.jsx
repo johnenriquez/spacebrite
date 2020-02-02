92ab269ea95a6909e9b7e58f3fd0cb9d2d96938d
John Enriquez <john.enriquez@gmail.com>
2020-02-01 22:21:09 -0800

    adds banner-cta

--- frontend/components/home/splash_banner.jsx
+++ frontend/components/home/splash_banner.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import FalseLink from '../common/false_link';
+import Icon from '../common/icon';
 
 const SplashBanner = () => (
-    <div className="splash-banner">
-        <div className="splash-banner-content">
-            <div className="container">
-                <h1>Explore our lowest fares to Mars</h1>
-                <p>Nonstop flights to Valles Marineris and Olympus Mons</p>
-                <FalseLink className="btn">Book Now</FalseLink>
+    <div className="splash-banner-container">
+        <div className="splash-banner">
+            <div className="splash-banner-content">
+                <div className="container">
+                    <h1>Explore our lowest fares to Mars</h1>
+                    <p>Nonstop flights to Valles Marineris and Olympus Mons</p>
+                    <FalseLink className="btn">Book Now</FalseLink>
+                </div>
             </div>
+        </div>
+        <div className="banner-cta">
+            <ul>
+                <li><FalseLink>Baggage fee changes <Icon className="fas fa-angle-right" /></FalseLink></li>
+                <li><FalseLink>Optional service fees <Icon className="fas fa-angle-right" /></FalseLink></li>
+            </ul>
         </div>
     </div>
 );
