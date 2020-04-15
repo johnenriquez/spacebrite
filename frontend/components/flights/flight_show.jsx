3875eae5cf44a363619d9397472906dba84e684b
John Enriquez <john.enriquez@gmail.com>
2020-04-14 20:12:27 -0700

    updates flight_show

--- frontend/components/flights/flight_show.jsx
+++ frontend/components/flights/flight_show.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
-const FlightShow = props => (
-    <div>Flight</div>
-);
+class FlightShow extends React.Component {
 
-export default FlightShow;
+    componentDidMount() {
+        window.scrollTo(0, 0);
+        this.props.fetchFlight();
+    }
+
+    render() {
+        const { flight } = this.props;
+        if (flight) {
+            return (
+                <div className="flight-show">
+                    Flight
+                </div>
+            );
+        } else {
+            return (
+                <div>Loading...</div>
+            );
+        }
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    let flightId = ownProps.match.params.flightId;
+    let flight = state.entities.flights[flightId];
+    return { flight }
+};
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    fetchFlight: () => dispatch(fetchFlight(ownProps.match.params.flightId)),
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(FlightShow);
